125a67f3b00f60a0b8a1229f25ea4e8083c3f770
luoyumo <yumo.luo@hotmail.com>
2022-01-01 23:34:57 +0200

    return after sending 400

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ app.put('/api/persons/:id', (req, res, next) => {
 app.post('/api/testing/reset', async (req, res) => {
   if (process.env.NODE_ENV !== 'test') {
     res.status(400).send('Can not reset db in production environment!')
+    return
   }
   await Person.deleteMany({})
   res.status(204).end()
